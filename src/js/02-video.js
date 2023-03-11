6bb864c3fb6e97dde82467ef8813a87a14a92545
IrynaKyslytsia <i.kislica89@gmail.com>
2023-03-11 18:24:10 +0200

    task 02 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,16 +4,17 @@ import throttle from 'lodash.throttle';
 const storageKey = "videoplayer-current-time";
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('timeupdate', _.throttle(updatePosition, 1000));
+const throttled = throttle(updatePosition, 1000);
 
-function updatePosition() {
-    localStorage.setItem(storageKey);
+player.on('timeupdate', throttled);
+
+function updatePosition({ seconds }) {
+    localStorage.setItem(storageKey, seconds);
 };
 
 const videoCurrentTime = localStorage.getItem(storageKey);
 
 player.setCurrentTime(videoCurrentTime);
-
 
