a88da7e07028332603a71c201e21d32d7613ae4e
IrynaKyslytsia <i.kislica89@gmail.com>
2023-03-11 15:47:52 +0200

    task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const storageKey = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', _.throttle(updatePosition, 1000));
+
+function updatePosition() {
+    localStorage.setItem(storageKey);
+};
+
+const videoCurrentTime = localStorage.getItem(storageKey);
+
+player.setCurrentTime(videoCurrentTime);
+
 
